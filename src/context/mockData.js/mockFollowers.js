9427733f48d6a2c075348ef974f11d0d834c0038
Bright Amidiagbe <captainamiedi1@gmail.com>
2021-01-04 23:19:37 +0100

    initial commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    "login": "olawalejarvis",
+    "id": 23451253,
+    "node_id": "MDQ6VXNlcjIzNDUxMjUz",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/23451253?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/olawalejarvis",
+    "html_url": "https://github.com/olawalejarvis",
+    "followers_url": "https://api.github.com/users/olawalejarvis/followers",
+    "following_url": "https://api.github.com/users/olawalejarvis/following{/other_user}",
+    "gists_url": "https://api.github.com/users/olawalejarvis/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/olawalejarvis/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/olawalejarvis/subscriptions",
+    "organizations_url": "https://api.github.com/users/olawalejarvis/orgs",
+    "repos_url": "https://api.github.com/users/olawalejarvis/repos",
+    "events_url": "https://api.github.com/users/olawalejarvis/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/olawalejarvis/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+];
